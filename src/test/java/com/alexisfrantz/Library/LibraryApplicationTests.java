57bff6a9b9208efb887d90ac48fc626a83a9fc9c
Alexis Frantz <loyallombardi@gmail.com>
2023-03-21 10:40:04 +0100

    initial setup

---
+++ src/test/java/com/alexisfrantz/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexisfrantz.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
